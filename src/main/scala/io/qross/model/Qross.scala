12ee8115d906969404ac664ce015a911399e9701
garfi-wu <garfi-wu@outlook.com>
2019-10-28 15:52:36 +0800

    v0.6.0-43-RELEASE indexes for tasks

--- src/main/scala/io/qross/model/Qross.scala
+++ src/main/scala/io/qross/model/Qross.scala
@@ -155,7 +155,6 @@ object Qross {
             writeDebugging("Disk info has been saved.")
         }
 
-        writeDebugging("Start running.")
         //clean mechanism
         if (now.matches(Global.CLEAN_TASK_RECORDS_FREQUENCY)) {
             writeDebugging("Cleaning tasks mechanism is ready to execute.")
@@ -164,8 +163,6 @@ object Qross {
                     VAR $TO_CLEAR := SELECT B.job_id, A.keep_x_task_records FROM qross_jobs A
                                         INNER JOIN (SELECT job_id, COUNT(0) AS task_amount FROM qross_tasks GROUP BY job_id) B ON A.id=B.job_id
                                             WHERE A.keep_x_task_records>0 AND B.task_amount>A.keep_x_task_records;
-
-                    PRINT DEBUG 'There are ' + @ROWS + ' jobs to clean.';
 
                     FOR $job_id, $keep_tasks IN $TO_CLEAR
                       LOOP
@@ -176,8 +173,6 @@ object Qross {
                             DELETE FILE @QROSS_HOME + "tasks/" + $job_id + "/" + ${ $create_time REPLACE "-" TO "" SUBSTRING 1 TO 9 } + "/" + $id + ".log";
                           END LOOP;
 
-                        PRINT DEBUG 'Stored log files of job ' + $job_id + ' has been deleted.';
-
                         DELETE FROM qross_tasks_logs WHERE job_id=$job_id AND task_id<=$task_id;
                         DELETE FROM qross_tasks_dependencies WHERE job_id=$job_id AND task_id<=$task_id;
                         DELETE FROM qross_tasks_dags WHERE job_id=$job_id AND task_id<=$task_id;
@@ -186,13 +181,11 @@ object Qross {
                         SET $rows := DELETE FROM qross_tasks WHERE job_id=$job_id AND id<=$task_id;
                         INSERT INTO qross_jobs_clean_records (job_id, amount, info) VALUES ($job_id, $rows, $task_id);
 
-                        PRINT DEBUG $rows + ' tasks of job $job_id has been deleted.';
+                        PRINT DEBUG $rows + ' tasks of job ' + $job_id + ' has been deleted.';
                       END LOOP;
                  """)
-            writeDebugging("Cleaning tasks mechanism has finished.")
+            writeDebugging("Tasks cleaning has finished.")
         }
-
-        writeDebugging("Stop running.")
         
         dh.close()
     }
