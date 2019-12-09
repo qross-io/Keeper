741658062a489d73d4c1da1ce10dda16019419ea
garfi-wu <garfi-wu@outlook.com>
2019-12-09 18:50:38 +0800

    v0.6.0-45-RELEASE tasks storer and cleaner update

---
+++ src/main/scala/io/qross/keeper/TaskLogStorer.scala
@@ -0,0 +1,30 @@
+package io.qross.keeper
+
+import io.qross.core.DataHub
+import io.qross.ext.Output.writeDebugging
+import io.qross.model.TaskStorage
+import io.qross.time.DateTime
+
+object TaskLogStorer {
+    def main(args: Array[String]): Unit = {
+
+        val dh = DataHub.QROSS
+        writeDebugging("Storing logs mechanism is ready to execute.")
+
+        dh.get(s"""SELECT job_id, id AS task_id, create_time FROM qross_tasks WHERE update_time<'${DateTime.now.minusDays(1).getString("yyyy-MM-dd HH:mm:00")}' AND saved='no'""")
+            .foreach(row => {
+                TaskStorage.store(
+                    row.getInt("job_id"),
+                    row.getLong("task_id"),
+                    row.getString("create_time"),
+                    dh)
+            }
+            )
+            .put("DELETE FROM qross_tasks_logs WHERE job_id=#job_id AND task_id=#task_id")
+            .put("UPDATE qross_tasks SET saved='yes' WHERE id=#task_id")
+
+        writeDebugging("Logs of " + dh.COUNT + " tasks has been stored.")
+
+        dh.close()
+    }
+}
--- src/main/scala/io/qross/model/Qross.scala
+++ src/main/scala/io/qross/model/Qross.scala
@@ -132,22 +132,22 @@ object Qross {
         }
 
         //store logs to disk every hour xx:27
-        if (now.matches(Global.CLEAN_TASK_LOGS_FREQUENCY)) {
-            writeDebugging("Storing logs mechanism is ready to execute.")
-            dh.get(s"""SELECT job_id, id AS task_id, create_time FROM qross_tasks WHERE update_time<'${now.minusDays(1).getString("yyyy-MM-dd HH:mm:00")}' AND saved='no'""")
-                    .foreach(row => {
-                        TaskStorage.store(
-                            row.getInt("job_id"),
-                            row.getLong("task_id"),
-                            row.getString("create_time"),
-                            dh)
-                        }
-                    )
-                    .put("DELETE FROM qross_tasks_logs WHERE task_id=#task_id")
-                    .put("UPDATE qross_tasks SET saved='yes' WHERE id=#task_id")
-            writeDebugging(dh.COUNT + " tasks has been stored.")
-            writeDebugging("Storing logs mechanism has finished.")
-        }
+//        if (now.matches(Global.CLEAN_TASK_LOGS_FREQUENCY)) {
+//            writeDebugging("Storing logs mechanism is ready to execute.")
+//            dh.get(s"""SELECT job_id, id AS task_id, create_time FROM qross_tasks WHERE update_time<'${now.minusDays(1).getString("yyyy-MM-dd HH:mm:00")}' AND saved='no'""")
+//                    .foreach(row => {
+//                        TaskStorage.store(
+//                            row.getInt("job_id"),
+//                            row.getLong("task_id"),
+//                            row.getString("create_time"),
+//                            dh)
+//                        }
+//                    )
+//                    .put("DELETE FROM qross_tasks_logs WHERE job_id=#job_id AND task_id=#task_id")
+//                    .put("UPDATE qross_tasks SET saved='yes' WHERE id=#task_id")
+//            writeDebugging(dh.COUNT + " tasks has been stored.")
+//            writeDebugging("Storing logs has finished.")
+//        }
 
         //disk space monitor
         if (now.matches(Global.DISK_MONITOR_FREQUENCY)) {
@@ -156,35 +156,37 @@ object Qross {
         }
 
         //clean mechanism
-        if (now.matches(Global.CLEAN_TASK_RECORDS_FREQUENCY)) {
-            writeDebugging("Cleaning tasks mechanism is ready to execute.")
-            dh.run(
-                """
-                    VAR $TO_CLEAR := SELECT B.job_id, A.keep_x_task_records FROM qross_jobs A
-                                        INNER JOIN (SELECT job_id, COUNT(0) AS task_amount FROM qross_tasks GROUP BY job_id) B ON A.id=B.job_id
-                                            WHERE A.keep_x_task_records>0 AND B.task_amount>A.keep_x_task_records;
-                    FOR $job_id, $keep_tasks IN $TO_CLEAR
-                      LOOP
-                        SET $task_id := SELECT id AS task_id FROM qross_tasks WHERE job_id=$job_id ORDER BY id DESC LIMIT $keep_tasks,1;
-
-                        FOR $id, $create_time IN (SELECT id, create_time FROM qross_tasks WHERE job_id=$job_id AND id<=$task_id)
-                          LOOP
-                            DELETE FILE @QROSS_HOME + "tasks/" + $job_id + "/" + ${ $create_time REPLACE "-" TO "" SUBSTRING 1 TO 9 } + "/" + $id + ".log";
-                          END LOOP;
-
-                        DELETE FROM qross_tasks_logs WHERE job_id=$job_id AND task_id<=$task_id;
-                        DELETE FROM qross_tasks_dependencies WHERE job_id=$job_id AND task_id<=$task_id;
-                        DELETE FROM qross_tasks_dags WHERE job_id=$job_id AND task_id<=$task_id;
-                        DELETE FROM qross_tasks_events WHERE job_id=$job_id AND task_id<=$task_id;
-                        DELETE FROM qross_tasks_records WHERE job_id=$job_id AND task_id<=$task_id;
-                        SET $rows := DELETE FROM qross_tasks WHERE job_id=$job_id AND id<=$task_id;
-                        INSERT INTO qross_jobs_clean_records (job_id, amount, info) VALUES ($job_id, $rows, $task_id);
-
-                        PRINT DEBUG $rows + ' tasks of job ' + $job_id + ' has been deleted.';
-                      END LOOP;
-                 """)
-            writeDebugging("Tasks cleaning has finished.")
-        }
+//        if (now.matches(Global.CLEAN_TASK_RECORDS_FREQUENCY)) {
+//            dh.run(
+//                """
+//                    PRINT 'Cleaning tasks mechanism is ready to execute.';
+//
+//                    VAR $TO_CLEAR := SELECT B.job_id, A.keep_x_task_records FROM qross_jobs A
+//                                        INNER JOIN (SELECT job_id, COUNT(0) AS task_amount FROM qross_tasks GROUP BY job_id) B ON A.id=B.job_id
+//                                            WHERE A.keep_x_task_records>0 AND B.task_amount>A.keep_x_task_records;
+//                    FOR $job_id, $keep_tasks IN $TO_CLEAR
+//                      LOOP
+//                        SET $task_id := SELECT id AS task_id FROM qross_tasks WHERE job_id=$job_id ORDER BY id DESC LIMIT $keep_tasks,1;
+//
+//                        FOR $id, $create_time IN (SELECT id, create_time FROM qross_tasks WHERE job_id=$job_id AND id<=$task_id)
+//                          LOOP
+//                            DELETE FILE @QROSS_HOME + "tasks/" + $job_id + "/" + ${ $create_time REPLACE "-" TO "" SUBSTRING 1 TO 9 } + "/" + $id + ".log";
+//                          END LOOP;
+//
+//                        DELETE FROM qross_tasks_logs WHERE job_id=$job_id AND task_id<=$task_id;
+//                        DELETE FROM qross_tasks_dependencies WHERE job_id=$job_id AND task_id<=$task_id;
+//                        DELETE FROM qross_tasks_dags WHERE job_id=$job_id AND task_id<=$task_id;
+//                        DELETE FROM qross_tasks_events WHERE job_id=$job_id AND task_id<=$task_id;
+//                        DELETE FROM qross_tasks_records WHERE job_id=$job_id AND task_id<=$task_id;
+//                        SET $rows := DELETE FROM qross_tasks WHERE job_id=$job_id AND id<=$task_id;
+//                        INSERT INTO qross_jobs_clean_records (job_id, amount, info) VALUES ($job_id, $rows, $task_id);
+//
+//                        PRINT DEBUG $rows + ' tasks of job ' + $job_id + ' has been deleted.';
+//                      END LOOP;
+//
+//                    PRINT 'Tasks cleaning has finished.';
+//                 """)
+//        }
         
         dh.close()
     }
