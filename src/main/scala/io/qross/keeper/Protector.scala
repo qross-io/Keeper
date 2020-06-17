8ada2ccdb7f00095c78b53d17ae8711f88bcaf35
garfi-wu <garfi-wu@outlook.com>
2020-06-17 17:51:35 +0800

    v0.6.2-38-SNAPSHOT

--- src/main/scala/io/qross/keeper/Protector.scala
+++ src/main/scala/io/qross/keeper/Protector.scala
@@ -2,14 +2,14 @@ package io.qross.keeper
 
 import io.qross.ext.Output
 import io.qross.model.KeeperLogger
-import io.qross.setting.Global
+import io.qross.setting.{Global, Properties}
 import io.qross.time.DateTime
 
 import scala.sys.process._
 
 object Protector {
-    def main(args: Array[String]): Unit = {
 
+    def main(args: Array[String]): Unit = {
         //val bash = if (Setting.HADOOP_AND_HIVE_ENABLED) "hadoop jar" else s"${Global.JAVA_BIN_HOME}java -cp"
         val bash = s"${Global.JAVA_BIN_HOME}java -cp"
         val command = s"$bash ${Global.QROSS_HOME}qross-keeper-${Global.QROSS_VERSION}.jar io.qross.keeper.Keeper ${Global.QROSS_HOME}qross.properties"
